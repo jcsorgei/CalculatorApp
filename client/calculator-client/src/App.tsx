8e63b5c71e7c31e4e965277ef7c5ca2a1f1f6220
jcsorgei <csjozef88@gmail.com>
2022-05-25 22:21:26 +0200

    feat(App.css,App.tsx): created the calculator layout

--- client/calculator-client/src/App.tsx
+++ client/calculator-client/src/App.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        <div className="display" style={{backgroundColor: "black"}} >5454+78</div>
+        <div className="AC" style={{gridColumn: "1 / 3"}}>AC</div>
+        <div className="DEL">DEL</div>     
+        <div className="op" style={{backgroundColor: "darkorange"}}>÷</div>
+        <div className="btn">1</div>
+        <div className="btn">2</div>
+        <div className="btn">3</div>
+        <div className="op" style={{backgroundColor: "darkorange"}}>*</div>
+        <div className="btn">4</div>
+        <div className="btn">5</div>
+        <div className="btn">6</div>
+        <div className="op" style={{backgroundColor: "darkorange"}}>+</div>
+        <div className="btn">7</div>
+        <div className="btn">8</div>
+        <div className="btn">9</div>
+        <div className="op" style={{backgroundColor: "darkorange"}}>-</div>
+        <div className="dec">.</div>
+        <div className="btn">0</div>
+        <div className="equal" style={{gridColumn: "3 / 5"}}>=</div>
     </div>
   );
 }
